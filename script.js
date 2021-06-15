718e86749159ae42c3be8872e0ef357cd0b0d17e
crypto-miner-official <85779881+crypto-miner-official@users.noreply.github.com>
2021-06-15 19:32:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function search(p) {
+    let item = document.getElementById('sme');
+    console.log(item.value.split(' '))
+    alert(item.value.split(' '))
+}
+// document.getElementsByClassName('card')[0].innerHTML = ''
