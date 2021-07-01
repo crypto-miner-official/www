3346cf426c770f4991e630ac030f8913cbc10b15
crypto-miner-official <85779881+crypto-miner-official@users.noreply.github.com>
2021-07-01 13:40:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,4 +4,26 @@ function search(p) {
     console.log(item.value.split(' '))
     alert(item.value.split(' '))
 }
+function pickrandomfromdict(param) {
+    var keys = Object.values(param['urlset']['url']);
+    return keys[keys.length * Math.random() << 0]['loc'];
+}
+function gotorandomblog() {
+    fetch('/sitemap.json')
+        .then(res => res.json())
+        .then((out) => {
+            let randomblog = pickrandomfromdict(out);
+            while (randomblog) {
+                if (randomblog == "https://crypto-miner.tech" || randomblog == "https://crypto-miner.tech/index.html" || randomblog == "https://crypto-miner.tech/mining" || randomblog == "https://crypto-miner.tech/budget-mining" || randomblog == "https://crypto-miner.tech/mobile" || randomblog == "https://crypto-miner.tech/other" || randomblog == "https://crypto-miner.tech/contact.html") {
+                    console.log('retrying')
+                    randomblog = pickrandomfromdict(out);
+                    continue;
+                } else {
+                    window.open(randomblog, '_blank')
+                    break;
+                }
+
+            }
+        }).catch(err => console.error(err));
+}
 // document.getElementsByClassName('card')[0].innerHTML = ''
